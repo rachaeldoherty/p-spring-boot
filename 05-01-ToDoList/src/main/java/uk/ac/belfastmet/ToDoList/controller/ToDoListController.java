c1ac8209517379e1374d5efa4505132011d3922f
rachaeldoherty <rachaeldoherty.fresher@users.noreply.github.com>
2019-09-23 15:30:28 +0100

    updated List website

--- 05-01-ToDoList/src/main/java/uk/ac/belfastmet/ToDoList/controller/ToDoListController.java
+++ 05-01-ToDoList/src/main/java/uk/ac/belfastmet/ToDoList/controller/ToDoListController.java
@@ -1,6 +1,5 @@
 package uk.ac.belfastmet.ToDoList.controller;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,33 +8,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import uk.ac.belfastmet.ToDoList.service.ToDoListService;
 
-
-
 @RequestMapping
 @Controller
 public class ToDoListController {
 
 	Logger logger = LoggerFactory.getLogger(ToDoListController.class);
-	
-	@RequestMapping(value="/", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String homePage(Model model) {
-		
 
 		ToDoListService toDoListService = new ToDoListService();
 		logger.info("Here is a warning message");
 		model.addAttribute("ToDoList", toDoListService.getToDoList());
+		model.addAttribute("message", "Spring Clean Time");
 		return "index";
 	}
 
-	@RequestMapping(value="/CompletedList", method=RequestMethod.GET)
-	public String completedList(Model model) {		
+	@RequestMapping(value = "/CompletedList", method = RequestMethod.GET)
+	public String completedList(Model model) {
 
 		ToDoListService toDoListService = new ToDoListService();
 		logger.info("Here is a warning message");
 		model.addAttribute("ToDoList", toDoListService.getcompletedList());
+		model.addAttribute("message", "Spring Clean Time - All Done!");
 		return "completed";
 	}
 }
-
-
 
--- 05-01-ToDoList/src/main/java/uk/ac/belfastmet/ToDoList/service/ToDoListService.java
+++ 05-01-ToDoList/src/main/java/uk/ac/belfastmet/ToDoList/service/ToDoListService.java
@@ -3,33 +3,30 @@ package uk.ac.belfastmet.ToDoList.service;
 import java.util.ArrayList;
 import uk.ac.belfastmet.ToDoList.domain.ToDoList;
 
-
 public class ToDoListService {
-	
+
 	private ArrayList<ToDoList> toDoList;
 
-	public ArrayList<ToDoList> getToDoList(){
+	public ArrayList<ToDoList> getToDoList() {
 		this.toDoList = new ArrayList<ToDoList>();
-		this.toDoList.add(new ToDoList("Kitchen", "Clean Kitchen", "25 September", false, "Rachael", "High")); 
-		this.toDoList.add(new ToDoList("Bathroom", "Spray Shower", "24 September", false, "Rachael", "Low")); 
-		this.toDoList.add(new ToDoList("Car", "Fill Tank", "26 September", true, "Rachael", "Medium")); 
-		this.toDoList.add(new ToDoList("Bedroom", "Hoover & Change Sheets", "23 September", false, "Rachael", "High")); 
-		
-				return this.toDoList;
+		this.toDoList.add(new ToDoList("Kitchen", "Clean Kitchen", "25 September", false, "Rachael", "High"));
+		this.toDoList.add(new ToDoList("Bathroom", "Spray Shower", "24 September", false, "Rachael", "Low"));
+		this.toDoList.add(new ToDoList("Car", "Fill Tank", "26 September", true, "Rachael", "Medium"));
+		this.toDoList.add(new ToDoList("Bedroom", "Hoover & Change Sheets", "23 September", false, "Rachael", "High"));
 
-}
+		return this.toDoList;
+
+	}
 
 	private ArrayList<ToDoList> completedList;
 
-	public ArrayList<ToDoList> getcompletedList(){
-		this. completedList = new ArrayList<ToDoList>();
-		this. completedList.add(new ToDoList("Car", "Fill Tank", "26 September", true, "Rachael", "Medium")); 
-		this. completedList.add(new ToDoList("Utility Room", "Organise Storage", "30 September", true, "Rachael", "Medium")); 
+	public ArrayList<ToDoList> getcompletedList() {
+		this.completedList = new ArrayList<ToDoList>();
+		this.completedList.add(new ToDoList("Car", "Fill Tank", "26 September", true, "Rachael", "Medium"));
+		this.completedList
+				.add(new ToDoList("Utility Room", "Organise Storage", "30 September", true, "Rachael", "Medium"));
 
-				return this.completedList;
+		return this.completedList;
 
-	
-	
+	}
 }
-}
-
